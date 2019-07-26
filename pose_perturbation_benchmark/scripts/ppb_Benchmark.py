1d23389b4bba5254b79eaaba21aeda29b8c4ea3b
YiHerngOng <ongyi@oregonstate.edu>
2019-07-26 14:56:08 -0700

    change roll and pitch s.t. fingers don't bump into table

--- pose_perturbation_benchmark/scripts/ppb_Benchmark.py
+++ pose_perturbation_benchmark/scripts/ppb_Benchmark.py
@@ -40,6 +40,8 @@ class ppBenchmark():
 		self._pIncrement = pIncrement # [xinc, yinc, zinc]
 		self._oIncrement = oIncrement # [rinc, pinc, winc]
 		self._basePose = base_pose # [x, y, z, r, p, w]
+		self.z = [0.06]
+
 		rospy.init_node("expo_demo", anonymous=True)
 		self.rate = rospy.Rate(10)
 		self.marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size=100)
@@ -47,48 +49,77 @@ class ppBenchmark():
 
 
 	def samplingPoses(self, ext, inc, axis, poses):
-		min_numPose = ext[0] / inc
-		# basepose = self._basePose[:]
+		# min_numPose = ext[0] / inc
 		if len(poses) == 0:
 			poses.append(self._basePose[:])
 		min_poses = deepcopy(poses)
 		max_poses = deepcopy(poses)
 
+
 		for pose in min_poses:
-			# if min_or_max == "min":
-			for i in range(int(min_numPose)):
+			if (axis == 3 or axis == 4) and pose[2] == self.z[0]:
+				min_numPose = 1
+				print ext[0]
+
+			elif (axis == 3 or axis == 4) and pose[2] == self.z[1]:
+				min_numPose = 2
+
+			else:
+				min_numPose = math.ceil(ext[0] / inc)
+
+			for _ in range(int(min_numPose)):
+				# print "min_numPose", min_numPose
 				pose[axis] -= inc
 				temp = pose[:]
 				poses.append(temp)
-				# print poses
-		max_numPose = ext[1] / inc
+
+		# max_numPose = ext[1] / inc
 		for pose in max_poses:
-			# if min_or_max == "max":
-			for _ in range(int(max_numPose)):
+			if (axis == 3 or axis == 4) and pose[2] == self.z[0]:
+				max_numPose = 1
+
+			elif (axis == 3 or axis == 4) and pose[2] == self.z[1]:
+				max_numPose = 2
+
+			else:
+				max_numPose = math.ceil(ext[1] / inc)
+				# if axis ==2:
+					# print max_numPose
+
+			for i in range(int(max_numPose)):
 				pose[axis] += inc
 				temp = pose[:]		
 				poses.append(temp)
-		# print poses
+
+		# get z list up-down
+		if axis == 2:
+			minbase_z = deepcopy(self._basePose[2])
+			for mdz in range(int(min_numPose)):
+				minbase_z -= inc
+				self.z.append(minbase_z)
+
+			maxbase_z = deepcopy(self._basePose[2])
+			for xdz in range(int(max_numPose)):
+				maxbase_z += inc
+				self.z.append(maxbase_z)
 
 		return poses
 
 	def sampling_all_Poses(self):
 		self.all_Poses = []
-		self.all_Poses = self.samplingPoses((self._pExtremes[0], self._pExtremes[1]), self._pIncrement[0], 0, self.all_Poses)
+		# self.all_Poses = self.samplingPoses((self._pExtremes[0], self._pExtremes[1]), self._pIncrement[0], 0, self.all_Poses)
 
-		self.all_Poses = self.samplingPoses((self._pExtremes[2],self._pExtremes[3]) , self._pIncrement[1], 1, self.all_Poses)
+		# self.all_Poses = self.samplingPoses((self._pExtremes[2],self._pExtremes[3]) , self._pIncrement[1], 1, self.all_Poses)
 
 		self.all_Poses = self.samplingPoses((self._pExtremes[4], self._pExtremes[5]),self._pIncrement[2], 2, self.all_Poses)
 
-		self.all_Poses = self.samplingPoses((self._oExtremes[0],self._oExtremes[1]), self._oIncrement[0], 3, self.all_Poses)
-		# self.all_Poses = self.samplingPoses(self._oExtremes[1], self._oIncrement[0], 3, self.all_Poses, "max")
+		# self.all_Poses = self.samplingPoses((self._oExtremes[0],self._oExtremes[1]), self._oIncrement[0], 3, self.all_Poses)
 
 		self.all_Poses = self.samplingPoses((self._oExtremes[2],self._oExtremes[3]), self._oIncrement[1], 4, self.all_Poses)
-		# self.all_Poses = self.samplingPoses(self._oExtremes[3], self._oIncrement[1], 4, self.all_Poses, "max")
 
-		self.all_Poses = self.samplingPoses((self._oExtremes[4], self._oExtremes[5]),self._pIncrement[2], 5, self.all_Poses)
-		# self.all_Poses = self.samplingPoses(self._oExtremes[5], self._pIncrement[2], 5, self.all_Poses, "max")
-
+		# self.all_Poses = self.samplingPoses((self._oExtremes[4], self._oExtremes[5]),self._pIncrement[2], 5, self.all_Poses)
+		# for i in self.all_Poses:
+		# 	print i
 	def rotation_matrix(self, r, p, w):
 		r_x = np.array([[1,0,0], [0, np.cos(math.radians(p)), -np.sin(math.radians(p))], [0,np.sin(math.radians(p)), np.cos(math.radians(p))]])
 		r_y = np.array([[np.cos(math.radians(r)), 0, np.sin(math.radians(r))], [0,1,0], [-np.sin(math.radians(r)), 0, np.cos(math.radians(r))]])
@@ -170,7 +201,7 @@ class ppBenchmark():
 		x = 0
 		y = 0
 		z = 0 
-		poses = self.all_Poses[0:100]
+		poses = self.all_Poses[:]
 		count = 0
 		for i in range(len(poses)):
 			# rot_mat = self.rotation_matrix(poses[i][3],poses[i][4],poses[i][5])
@@ -187,10 +218,10 @@ class ppBenchmark():
 					r = 0 + poses[i][4]
 					p = 0 + poses[i][3]
 					w = -90 + poses[i][5]
-				print count
+				# print count
 				arrow.markers.append(self.add_marker(poses[i][0],poses[i][1],poses[i][2],r,p,w,count))
 				count += 1
-				print poses[i][5]
+				# print poses[i][5]
 		# print self.all_Poses[0:100]
 		# print arrow.markers[0]
 		while not rospy.is_shutdown():
@@ -199,9 +230,9 @@ class ppBenchmark():
 
 	
 if __name__ == '__main__':
-	pExt = [0.06, 0.06, 0.0, 0.04, 0.0, 0.04]
-	oExt = [45, 45, 0, 45, 30, 30]
-	pInc = [0.02,0.02,0.02]
+	pExt = [0.6, 0.6, 0.0, 0.4, 0.0, 0.6]
+	oExt = [45, 45, 45, 45, 30, 30]
+	pInc = [0.2,0.2,0.2]
 	oInc = [15,15,15]
 
 	qx = 0.73280044528
@@ -210,15 +241,19 @@ if __name__ == '__main__':
 	qw = 0.679648051136
 	angles = transformations.euler_from_quaternion([qx, qy, qz, qw])
 	home_joints = [4.709939211146766, 2.8400415518300606, -2.3948526894673706e-05, 0.7500912370325066, -1.6632760154433166, 4.480076660920214, 17.446434920234484]
-	base_pose = [0.0047,-0.5159,0.072648,0.73280044528,0.0286624951588,-0.0161398685441,0.679648051136]
+	# base_pose = [0.0047,-0.5159,0.072648,0.73280044528,0.0286624951588,-0.0161398685441,0.679648051136]
+	base_pose = [0,-0.54,0.06,0,0,0]
 
 	lift_pose = [0.04250594322, -0.575295301029, 0.332918595279, 0.558853317466, 0.459234398492, 0.396013037662, 0.565650431628]
 	shake_pose1 = [0.0424577485112, -0.575276284964, 0.332864738195, 0.682842077878, 0.238865152403, 0.566419484376, 0.394764617672]
 	shake_pose2 = [0.0424396061673, -0.575292909309, 0.332909875925, 0.29602066861, 0.660033261061, 0.102708646288, 0.682772870014]
-	# ppB = ppBenchmark(base_pose, pExt, oExt, pInc, oInc)
-	# ppB.sampling_all_Poses()
+	ppB = ppBenchmark(base_pose, pExt, oExt, pInc, oInc)
+	ppB.sampling_all_Poses()
+	for i in ppB.all_Poses:
+		print i
+	print ppB.z
 
-	# ppB.add_marker_poses()
+	ppB.add_marker_poses()
 	# define robot and move to base pose
 
 	# print Robot.group.get_current_joint_values()
@@ -226,15 +261,15 @@ if __name__ == '__main__':
 	# pitch - x -90, -45, -60, -120, -135
 	# yaw - z 180, 165, 150, 195 210
 	# base_pose = [0.0,-0.55,0.07, -90, 0, 180]
-	next_pose = [0.0,-0.59,0.07, -90, 0, 180]
-	lift_pose = [0.0,-0.59,0.2, -90, 0, 180]
+	# next_pose = [0.0,-0.59,0.07, -90, 0, 180]
+	# lift_pose = [0.0,-0.59,0.2, -90, 0, 180]
 
-	jpose = [0.9738466077,1.5756331639,-2.3710322135,1.0999663433,-0.4126239352,1.5473627854,-0.8734530441]
-	vertical_pose = [3.1414, 3.14, 3.1414, 3.141410729179159, 3.14, 3.141534807922412, 3.14]
+	# jpose = [0.9738466077,1.5756331639,-2.3710322135,1.0999663433,-0.4126239352,1.5473627854,-0.8734530441]
+	# vertical_pose = [3.1414, 3.14, 3.1414, 3.141410729179159, 3.14, 3.141534807922412, 3.14]
 
-	Robot = robot("kinova")
-	Robot.planner_type("RRT*")
-	Robot.move_to_Joint(base_pose)
+	# Robot = robot("kinova")
+	# Robot.planner_type("RRT*")
+	# Robot.move_to_Joint(base_pose)
 	# Robot.move_to_waypoint(lift_pose, "goal")
 	# Robot.display_Trajectory()
 	# Robot.move_to_waypoint(jpose, "pose")
